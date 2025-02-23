9a05a4f11ba8b3bb9959602d8bb8ebb0e5da8e0f
Cédric Belin <cedric@belin.io>
2025-02-23 02:15:22 +0100

    Add the `ResultSet` class

---
+++ src/result_set.cs
@@ -0,0 +1,40 @@
+namespace Belin.Which;
+
+/// <summary>
+/// Provides convenient access to the stream of search results.
+/// </summary>
+/// <param name="command">The searched command.</param>
+/// <param name="finder">The finder used to perform the search.</param>
+public class ResultSet(string command, Finder finder) {
+	
+	/// <summary>
+	/// The searched command.
+	/// </summary>
+	private readonly string command = command;
+
+	/// <summary>
+	/// The finder used to perform the search.
+	/// </summary>
+	private readonly Finder finder = finder;
+
+	/// <summary>
+	/// All instances of the searched command.
+	/// </summary>
+	public string[] All {
+		get => [.. Stream.Distinct()];
+	}
+
+	/// <summary>
+	/// The first instance of the searched command.
+	/// </summary>
+	public string? First {
+		get => Stream.FirstOrDefault();
+	}
+
+	/// <summary>
+	/// A stream of instances of the searched command.
+	/// </summary>
+	public IEnumerable<string> Stream {
+		get => finder.Find(command);
+	}
+}
