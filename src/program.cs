ebbcb6a88f4985da1982be5a71f73e4857c91a13
Cédric Belin <cedric@belin.io>
2025-02-23 16:48:43 +0100

    Code formatting

--- src/program.cs
+++ src/program.cs
@@ -1,20 +1,8 @@
 using System.CommandLine;
 using static Belin.Which.Finder;
 
-// Configure the command line arguments.
-var commandArgument = new Argument<string>("command", "The name of the executable to find.");
-var allOption = new Option<bool>(["-a", "--all"], "List all executable instances found (instead of just the first one).");
-var silentOption = new Option<bool>(["-s", "--silent"], "Silence the output, just return the exit code (0 if any executable is found, otherwise 404).");
-
-var program = new RootCommand("Find the instances of an executable in the system path.") {
-	commandArgument,
-	allOption,
-	silentOption
-};
-
-// Configure the command line handler.
-program.SetHandler((command, all, silent) => {
-
+// The program handler.
+Func<string, bool, bool, Task<int>> handler = (command, all, silent) => {
 	var resultSet = Which(command);
 	var executables = new List<string>();
 	if (all) executables.AddRange(resultSet.All);
@@ -30,8 +18,18 @@ program.SetHandler((command, all, silent) => {
 
 	if (!silent) Console.Error.WriteLine($"No \"{command}\" in ({string.Join(Path.PathSeparator, resultSet.Finder.Paths)}).");
 	return Task.FromResult(404);
+};
 
-}, commandArgument, allOption, silentOption);
+// Configure the command line.
+var commandArgument = new Argument<string>("command", "The name of the executable to find.");
+var allOption = new Option<bool>(["-a", "--all"], "List all executable instances found (instead of just the first one).");
+var silentOption = new Option<bool>(["-s", "--silent"], "Silence the output, just return the exit code (0 if any executable is found, otherwise 404).");
+var program = new RootCommand("Find the instances of an executable in the system path.") {
+	commandArgument,
+	allOption,
+	silentOption
+};
 
 // Start the application.
+program.SetHandler(handler, commandArgument, allOption, silentOption);
 return program.Invoke(args);
