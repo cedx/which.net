3c364cc622c1498b2855bcd2c50cfd3e3a431084
Cédric Belin <cedx@outlook.com>
2025-07-28 00:04:35 +0200

    Code formatting

--- run.ps1
+++ run.ps1
@@ -1,6 +1,5 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
-
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
 & dotnet "$scriptRoot/bin/Belin.Which.dll" @args
