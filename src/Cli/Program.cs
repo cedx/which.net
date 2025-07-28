03be648e6bb5c1527aa327c9860afdb869a3a544
Cédric Belin <cedx@outlook.com>
2025-07-28 22:46:28 +0200

    Set the text of the console title bar

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,4 +1,9 @@
 using Belin.Which.Cli;
+using System.Reflection;
+
+// Set the text of the console title bar.
+var assembly = typeof(Program).Assembly;
+Console.Title = assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
 
 // Start the application.
 return new RootCommand().Parse(args).Invoke();
