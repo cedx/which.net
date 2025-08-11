4728dd7e8aef84376303f2ad787f4cb207a47863
Cédric Belin <cedx@outlook.com>
2025-08-11 22:23:52 +0200

    Remove the `ResultSet.Stream` property, implement the `IEnumerable<string>` interface

--- example/Iteration.cs
+++ example/Iteration.cs
@@ -3,4 +3,4 @@ using static Belin.Which.Finder;
 
 // Finds all instances of an executable and returns them one at a time.
 Console.WriteLine("The 'foobar' command is available at these locations:");
-foreach (var path in Which("foo").Stream) Console.WriteLine($"- {path}");
+foreach (var path in Which("foo")) Console.WriteLine($"- {path}");
--- src/ResultSet.cs
+++ src/ResultSet.cs
@@ -1,24 +1,33 @@
 namespace Belin.Which;
 
+using System.Collections;
+
 /// <summary>
 /// Provides convenient access to the stream of search results.
 /// </summary>
 /// <param name="command">The searched command.</param>
 /// <param name="finder">The finder used to perform the search.</param>
-public sealed class ResultSet(string command, Finder finder) {
+public sealed class ResultSet(string command, Finder finder): IEnumerable<string> {
 
 	/// <summary>
 	/// All instances of the searched command.
 	/// </summary>
-	public string[] All => [.. Stream.Distinct()];
+	public string[] All => [.. finder.Find(command).Distinct()];
 
 	/// <summary>
 	/// The first instance of the searched command. Returns <see langword="null"/> if not found.
 	/// </summary>
-	public string? First => Stream.FirstOrDefault();
+	public string? First => finder.Find(command).FirstOrDefault();
 
 	/// <summary>
-	/// A stream of instances of the searched command.
+	/// Returns an enumerator that iterates through the sort properties.
 	/// </summary>
-	public IEnumerable<string> Stream => finder.Find(command);
+	/// <returns>An enumerator that can be used to iterate through the sort properties.</returns>
+	public IEnumerator<string> GetEnumerator() => finder.Find(command).GetEnumerator();
+
+	/// <summary>
+	/// Returns an enumerator that iterates through the sort properties.
+	/// </summary>
+	/// <returns>An enumerator that can be used to iterate through the sort properties.</returns>
+	IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
