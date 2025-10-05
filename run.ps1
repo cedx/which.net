f4ebc5df4031206de8ff6c413c377850c3bc67e8
Cédric Belin <cedx@outlook.com>
2025-10-05 13:15:51 +0200

    Update th launch script [skip ci]

--- run.ps1
+++ run.ps1
@@ -1,5 +1,8 @@
 #!/usr/bin/env pwsh
 Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
 & dotnet "$scriptRoot/bin/Belin.Which.dll" @args
