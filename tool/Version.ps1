f20d6af9913f6f48e9e8de1ff5fff1d0f18b69be
Cédric Belin <cedx@outlook.com>
2025-10-06 21:55:27 +0200

    Code formatting [skip ci]

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,4 +1,6 @@
 Write-Host "Updating the version number in the sources..."
 $version = [xml] (Get-Content "Package.xml") | Select-Xml "//Version"
 (Get-Content "Setup.iss") -replace 'version "\d+(\.\d+){2}.*"', "version ""$version""" | Out-File "Setup.iss"
-foreach ($item in Get-ChildItem "*/*.csproj") { (Get-Content $item) -replace "<Version>\d+(\.\d+){2}.*</Version>", "<Version>$version</Version>" | Out-File $item }
+foreach ($item in Get-ChildItem "*/*.csproj") {
+	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}.*</Version>", "<Version>$version</Version>" | Out-File $item
+}
