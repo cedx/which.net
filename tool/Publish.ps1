d5c96172644a6b1064f4a76049dbeab499a0a535
Cédric Belin <cedx@outlook.com>
2025-10-06 12:29:30 +0200

    Move the "Package.xml" file to the project root [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -2,7 +2,7 @@
 . tool/Version.ps1
 
 Write-Host "Publishing the package..."
-$version = [xml] (Get-Content "etc/Package.xml") | Select-Xml "//Version"
+$version = [xml] (Get-Content "Package.xml") | Select-Xml "//Version"
 git tag "v$version"
 git push origin "v$version"
 
--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,4 +1,4 @@
 Write-Host "Updating the version number in the sources..."
-$version = [xml] (Get-Content "etc/Package.xml") | Select-Xml "//Version"
+$version = [xml] (Get-Content "Package.xml") | Select-Xml "//Version"
 (Get-Content "Setup.iss") -replace 'version "\d+(\.\d+){2}.*"', "version ""$version""" | Out-File "Setup.iss"
 foreach ($item in Get-ChildItem "*/*.csproj") { (Get-Content $item) -replace "<Version>\d+(\.\d+){2}.*</Version>", "<Version>$version</Version>" | Out-File $item }
