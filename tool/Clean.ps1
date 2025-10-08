4d028458c4cf1304d601168280fab163ff20c165
Cédric Belin <cedx@outlook.com>
2025-10-09 00:01:43 +0200

    Code optimization [skip ci]

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,4 +1,4 @@
 Write-Host "Deleting all generated files..."
 if (Test-Path "bin") { Remove-Item "bin" -Force -Recurse }
-foreach ($item in Get-ChildItem "*/obj") { Remove-Item $item -Force -Recurse }
+Remove-Item "*/obj" -Force -Recurse
 Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse
