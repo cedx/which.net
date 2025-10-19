54c52804fc5be6853a6345e3928f0166f65269b4
Cédric Belin <cedx@outlook.com>
2025-10-19 17:02:08 +0200

    Code formatting

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -6,8 +6,8 @@ $version = (Import-PowerShellDataFile "Which.psd1").ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 
-dotnet pack --output=var
+dotnet pack --output var
 foreach ($item in Get-ChildItem "var/*.nupkg") {
-	dotnet nuget push $item --api-key=$Env:NUGET_API_KEY --source=https://api.nuget.org/v3/index.json
-	dotnet nuget push $item --api-key=$Env:GITHUB_TOKEN --source=https://nuget.pkg.github.com/cedx/index.json
+	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+	dotnet nuget push $item --api-key $Env:GITHUB_TOKEN --source https://nuget.pkg.github.com/cedx/index.json
 }
