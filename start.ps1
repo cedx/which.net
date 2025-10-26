23505247a58058fccd3d1472a9f010662595c01d
Cédric Belin <cedx@outlook.com>
2025-10-27 00:17:38 +0100

    Code formatting [skip ci]

--- start.ps1
+++ start.ps1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
-Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
+Set-StrictMode -Version Latest
 
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
