e34a9d58a39b195dfbe739d55632163ad6551877
Cédric Belin <cedx@outlook.com>
2025-11-10 20:14:46 +0100

    Update the launch script [skip ci]

--- Start.ps1
+++ Start.ps1
@@ -2,7 +2,4 @@
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 Set-StrictMode -Version Latest
-
-$commandPath = Get-Item $PSCommandPath
-$scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
-dotnet run --project "$scriptRoot/src" -- @args
+dotnet run --project "$PSScriptRoot/src" -- @args
