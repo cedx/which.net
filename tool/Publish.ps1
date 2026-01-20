c73334d0de1c69956301a5e3f2e1ba85505e52e5
Cédric Belin <cedx@outlook.com>
2026-01-20 21:09:45 +0100

    Code formatting [skip ci]

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -8,11 +8,11 @@ else {
 }
 
 "Publishing the package..."
-$version = (Import-PowerShellDataFile "Which.psd1").ModuleVersion
+$version = Import-PowerShellDataFile Which.psd1 | Select-Object -ExpandProperty ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 
 dotnet pack --output var
-foreach ($item in Get-Item "var/*.nupkg") {
-	dotnet nuget push $item --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+Get-Item var/*.nupkg | ForEach-Object {
+	dotnet nuget push $_ --api-key $Env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
 }
